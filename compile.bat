57d9c9a52d1cb9a99a8520002d811c195d854dc5
Enric Castella <ecastella@ayesa.com>
2020-04-11 18:17:59 +0200

    Compiler for all platforms

--- compile.bat
+++ compile.bat
@@ -1,11 +1,19 @@
 set GOOS=linux
 set GOARCH=amd64
-go build -o ./build/linux64/check-before-deploy-linux check-before-deploy.go
+go build -o ./build/linux64/check-before-deploy-linux64 check-before-deploy.go
+
+set GOOS=linux
+set GOARCH=386
+go build -o ./build/linux32/check-before-deploy-linux32 check-before-deploy.go
 
 set GOOS=darwin
 set GOARCH=amd64
 go build -o ./build/osx/check-before-deploy-mac check-before-deploy.go
 
 set GOOS=windows
+set GOARCH=386
+go build -o ./build/win32/check-before-deploy-win32.exe check-before-deploy.go
+
+set GOOS=windows
 set GOARCH=amd64
-go build -o ./build/win64/check-before-deploy-win.exe check-before-deploy.go
+go build -o ./build/win64/check-before-deploy-win64.exe check-before-deploy.go
